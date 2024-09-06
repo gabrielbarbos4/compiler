81af83c51a21327c16b7d029d2c44911eb6e9d57
gabriel barbosa <gabrielarraesbarbosa@gmail.com>
2024-09-06 18:37:23 -0300

    completed lexical

--- src/lexical.js
+++ src/lexical.js
@@ -2,17 +2,35 @@
 
 let currentIndex = 0;
 const TOKENS = new Set();
-const ignore_characters = new Set("\n", "");
+const ignore_characters = [" ", "\t", "\r", "\n"];
 const OPERATORS = [
-  { name: "assignment", value: "=" }
-]
+  { name: "assignment", value: "=" },
+  { name: "add", value: "+" },
+  { name: "subtract", value: "-" },
+  { name: "multiply", value: "*" },
+  { name: "divide", value: "/" },
+  { name: "modulo", value: "%" },
+  { name: "eq", value: "==" },
+  { name: "ne", value: "!=" },
+  { name: "gt", value: ">" },
+  { name: "lt", value: "<" },
+  { name: "ge", value: ">=" },
+  { name: "le", value: "<=" },
+];
 const ACTIONS = [
-  { name: "rem", value: "rem" }
+  { name: "rem", value: "rem" },
+  { name: "input", value: "input" },
+  { name: "let", value: "let" },
+  { name: "print", value: "print" },
+  { name: "goto", value: "goto" },
+  { name: "if", value: "if" },
+  { name: "enter", value: "\n" },
+  { name: "end", value: "end" }
 ]
 
 const analyze = (fileString) => {
 
-  while(currentIndex != fileString.length) {
+  while(currentIndex !== fileString.length) {
     const character = fileString.charAt(currentIndex);
     let validated = false;
 
@@ -23,15 +41,23 @@ const analyze = (fileString) => {
 
     if(!validated && isDigit(character)) {
       let digit = character
-      const nextChar = fileString.charAt(currentIndex + 1);
+      let nextChar = fileString.charAt(currentIndex + 1);
 
       while(isDigit(nextChar)) {
         digit += nextChar;
         currentIndex++;
+        nextChar = fileString.charAt(currentIndex + 1);
       }
 
+      if(!isIgnore(nextChar))
+        throw new Error(`Nao eh ignore char apos um numero`)
+
       TOKENS.add({ type: "integer", value: digit });
+      currentIndex++;
+      validated = true;
     }
+
+    isUppercase(character)
 
     if(!validated && isOperator(character)) {
       let finalOperator = character;
@@ -41,10 +67,12 @@ const analyze = (fileString) => {
         finalOperator += nextChar;
         currentIndex += 2;
       } else if(!isIgnore(nextChar)) {
-        throw new Error("Caracter invalido apos operador")
+        throw new Error(`Caracter invalido apos operador: ${nextChar}`)
       }
 
       TOKENS.add({ type: "operator", value: finalOperator });
+      currentIndex++;
+      validated = true;
     }
 
     const nextChar = fileString.charAt(currentIndex + 1);
@@ -53,32 +81,49 @@ const analyze = (fileString) => {
       const actionValidation = isAction(character, fileString, currentIndex);
 
       if(actionValidation.isAction) {
-        currentIndex += actionValidation.walkedIndexCounter
-        TOKENS.add({ type: "action", value: actionValidation.moutedWord })   
+        currentIndex += actionValidation.walkedIndexCounter;
+
+        TOKENS.add({ type: "action", value: actionValidation.moutedWord });
+        validated = true;
       }
     }
 
-    if(!validated && nextChar === " ") {
-      // TODO validar se o caracter eh minusculo
+    if((!validated && isIgnore(nextChar) || nextChar === "\n") && /^[a-z]$/.test(character)) {
       TOKENS.add({ type: "variable", value: character });
+      currentIndex++;
+      validated = true;
     }
 
-    if(!validated) throw new Error("fora da gramatica")
+    if(!validated) 
+      throw new Error(`fora da gramatica: ${character}, next: ${nextChar}`);
   }
 
   TOKENS.forEach(token => console.log(token));
 }
 
 const isAction = (character, fileString, currentIndex) => {
-  const possibleAction = ACTIONS.find(action => action.value.charAt(0) === character);
+  // TODO validar REM
+  let possibleAction;
 
-  if(!possibleAction) return { isAction: false }
+  if(character === "i") {
+    possibleAction = ACTIONS.find(
+      action => action.value.charAt(0) === character
+      && action.value.charAt(1) === fileString.charAt(currentIndex + 1)
+    )
+  } else {
+    possibleAction = ACTIONS.find(action => action.value.charAt(0) === character)
+  }
 
-  const moutedWord = character;
-  const walkedIndexCounter = 0;
+  if(!possibleAction) 
+    return { isAction: false }
+
+  let moutedWord = character;
+  let walkedIndexCounter = 0;
 
   for(let i = 0; i < possibleAction.value.length - 1; i++) {
-    moutedWord += fileString.charAt(currentIndex + 1);
+    const nextChar = fileString.charAt(currentIndex + 1);
+    isUppercase(nextChar);   
+    moutedWord += nextChar;
 
     currentIndex++;
     walkedIndexCounter++;
@@ -86,7 +131,10 @@ const isAction = (character, fileString, currentIndex) => {
 
   walkedIndexCounter++;
 
-  if(moutedWord !== possibleAction.value) throw new Error("Token nao presente na gramatica")
+  const nextChar = fileString.charAt(currentIndex + 1);
+
+  if(moutedWord !== possibleAction.value || !isIgnore(nextChar)) 
+    throw new Error(`Acao nao presente na gramatica ${nextChar}`)
 
   return {
     walkedIndexCounter,
@@ -96,8 +144,7 @@ const isAction = (character, fileString, currentIndex) => {
 }
 
 const isOperator = (character) => {
-  return OPERATORS
-    .find(operator => operator.value === character) !== undefined
+  return OPERATORS.find(operator => operator.value === character) !== undefined
 }
 
 const isDigit = (character) => {
@@ -105,7 +152,12 @@ const isDigit = (character) => {
 }
 
 const isIgnore = (character) => {
-  return ignore_characters.has(character) 
+  return ignore_characters.find(c => c === character) !== undefined 
+}
+
+const isUppercase = (character) => {
+  if(/^[A-Z]$/.test(character))
+    throw new Error(`Caracter maiusculo nao eh valido`)
 }
 
 module.exports = { analyze }
